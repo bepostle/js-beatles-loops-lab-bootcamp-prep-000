9a8a9beacee88fcfb9d3e53a402caf5fd544eff0
Brent Postlethweight <brentpostle@gmail.com>
2018-09-06 03:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ function theBeatlesPlay (musicians, instruments) {
 }
 
 function johnLennonFacts (facts) {
-  let facts = []
+  facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]
   let i = 0;
   while (i <= facts.length) {
     facts[i] = (facts[i] + '!!!');
